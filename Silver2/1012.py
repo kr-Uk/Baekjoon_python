0a5ca0abb3dd3d12106f4fb2fb61ae215040a644
KIMMINUK <kmwpcpk@gmail.com>
2025-03-27 23:53:25 +0900

    0327

--- Silver2/1012.py
+++ Silver2/1012.py
@@ -11,29 +11,42 @@
 [0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 count = 3 ...
+아ㅏ 이렇게 하면 그 사이에 있는게 카운트될 수도 있구나
+"""
+
+"""
+BFS 또는 DFS를 하는 문제구나,, !!
 """
 
 import sys
+sys.setrecursionlimit(10**6)
+
+def dfs(x, y):
+    dx = [0, 0, -1, 1]
+    dy = [-1, 1, 0, 0]
+    
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if (0 <= nx < m) and (0 <= ny < n) and graph[ny][nx] == 1:
+            graph[ny][nx] = -1
+            dfs(nx, ny)
 
 t = int(input())
-result = []
 
 for _ in range(t):
     m, n, k = map(int, sys.stdin.readline().split())
-    ilist = [[0] * (n+2) for _ in range(m+2)]
-    count = 0
+    graph = [[0 for _ in range(m)] for _ in range(n)]
     
-    for i in range(k):
-        a, b = map(int, sys.stdin.readline().split())
-        if ilist[a+1][b+1] == 0:
-            count += 1
-        ilist[a+1][b+1] = 1
-        ilist[a+1][b+2] = 1
-        ilist[a+2][b+1] = 1
-        ilist[a+1][b] = 1
-        ilist[a][b+1] = 1
-    result.append(count)
-        
-for i in result:
-    print(i)
-            
+    for _ in range(k):
+        y, x = map(int, sys.stdin.readline().split())
+        graph[x][y] = 1
+    
+    count = 0
+    for _y in range(m):
+        for _x in range(n):
+            if graph[_x][_y] == 1:
+                dfs(_y, _x)
+                count += 1
+
+    print(count)
