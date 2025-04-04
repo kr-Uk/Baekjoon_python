d21c492f81a5a7d9818b89e4826edaa028971c07
KIMMINUK <kmwpcpk@gmail.com>
2025-04-04 20:03:45 +0900

    0404

--- Silver1/1149.py
+++ Silver1/1149.py
@@ -1,19 +1,26 @@
 """
-dp인듯 !
 연속된 색 x
 R R
 R G G
 R G R G R // O
+모든 경우의 수를 빠르게 확인..?
+최소 비용 신장 트리,,
+[26 40 83]
+[49+40 60+26 57+26]
+[13+57+26 89+57+26 99+60+26]
 """
 
 import sys
 input = sys.stdin.readline
 
 n = int(input())
+rgb = [[] for _ in range(n)]
+rgb[0] = list(map(int, input().split()))
 
-rgb = [list(map(int, input().split()) for _ in range(n))]
-dp = [0 for _ in range(n)]
+for i in range(1, n):
+    rgb[i] = list(map(int, input().split()))
+    rgb[i][0] += min(rgb[i-1][1], rgb[i-1][2])
+    rgb[i][1] += min(rgb[i-1][0], rgb[i-1][2])
+    rgb[i][2] += min(rgb[i-1][0], rgb[i-1][1])
 
-for i in range(n):
-    if i == 0:
-        pass
+print(min(rgb[n-1]))
