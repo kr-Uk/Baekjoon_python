8ae9ae45676ec969dee9362573fbc0570a5168db
KIMMINUK <kmwpcpk@gmail.com>
2025-04-07 16:35:07 +0900

    0407

--- Silver1/10844.py
+++ Silver1/10844.py
@@ -32,5 +32,12 @@ input = sys.stdin.readline
 
 n = int(input())
 dp = [[0 for _ in range(10)] for _ in range(n)]
+dp[0][0] = 0
+for i in range(1, 10):
+    dp[i][0] = 1
 
+for i in range(1, n):
+    for j in range(10):
+        dp[j][i] = dp # 여기
+        
 print(dp[n-1] % 1000000000)
