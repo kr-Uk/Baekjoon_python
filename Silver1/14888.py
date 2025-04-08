f9da921e4f4369b7b9fa7584c876dadb83799cbc
KIMMINUK <kmwpcpk@gmail.com>
2025-04-08 20:02:14 +0900

    0408

--- Silver1/14888.py
+++ Silver1/14888.py
@@ -1,7 +1,5 @@
 """
-최대 10개의 연산자
-10! / 중복!
-"""
+시간초과 ! 처음 생각한 방법
 
 import sys
 import itertools
@@ -34,6 +32,51 @@ for i in list(itertools.permutations(op, n-1)):
         maximum = result
     if result < minimum:
         minimum = result
+
+print(maximum)
+print(minimum)
+"""
+
+import sys
+import itertools
+input = sys.stdin.readline
+maximum = -1e9
+minimum = 1e9
+
+n = int(input())
+seq = list(map(int, input().split()))
+op_num = list(map(int, input().split()))
+
+def dfs(k, temp):
+    global maximum, minimum
+    
+    # 종료 조건
+    if k == n-1:
+        maximum = max(temp, maximum)
+        minimum = min(temp, minimum)
+        return
+    
+    if op_num[0] != 0:
+        op_num[0] -= 1
+        dfs(k+1, temp + seq[k+1])
+        op_num[0] += 1
+    
+    if op_num[1] != 0:
+        op_num[1] -= 1
+        dfs(k+1, temp - seq[k+1])
+        op_num[1] += 1
+        
+    if op_num[2] != 0:
+        op_num[2] -= 1
+        dfs(k+1, temp * seq[k+1])
+        op_num[2] += 1
+    
+    if op_num[3] != 0:
+        op_num[3] -= 1
+        dfs(k+1, int(temp / seq[k+1]))
+        op_num[3] += 1
+
+dfs(0, seq[0])
 
 print(maximum)
 print(minimum)
