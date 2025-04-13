4bf2810c6549d018bb0d58bc86a7f06882a8a717
KIMMINUK <kmwpcpk@gmail.com>
2025-04-13 18:48:32 +0900

    0413

--- Gold5/12865.py
+++ Gold5/12865.py
@@ -35,6 +35,13 @@ input = sys.stdin.readline
 
 n, k = map(int, input().split())
 wv = [list(map(int, input().split())) for _ in range(n)]
-dp = [0] * (n+1)
+dp = [[0] * (k+1) for _ in range(n+1)]
 
-
+for i in range(1, n+1):
+    for j in range(1, k+1):
+        if j >= wv[i-1][0]:
+            dp[i][j] = max(wv[i-1][1] + dp[i-1][j-wv[i-1][0]], dp[i-1][j])
+        else:
+            dp[i][j] = dp[i-1][j]
+            
+print(dp[n][k])
