001253d5c083898636268548f0c1bfd1ff880666
KIMMINUK <kmwpcpk@gmail.com>
2025-04-22 16:35:51 +0900

    0422

---
+++ Gold5/1074.py
@@ -0,0 +1,9 @@
+"""
+별찍기처럼 재귀를 써서 한번 할 때마다 cnt+1
+"""
+
+import sys
+input = sys.stdin.readline
+
+N, r, c = map(int, input().split())
+
--- Gold5/15686.py
+++ Gold5/15686.py
@@ -12,9 +12,43 @@ bfs로 탐색
 사실 조합 사용하면 편하긴 함..!
 """
 
-import itertools
 import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
+result = sys.maxsize
+coor_2 = []
+coor_1 = []
+
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] == 1:
+            coor_1.append((i, j))
+        elif graph[i][j] == 2:
+            coor_2.append((i, j))
+
+visited = [False] * len(coor_2)
+
+def dfs(idx, cnt):
+    global result
+    if cnt == m:
+        ans = 0
+        for y, x in coor_1:
+            min_distance = 9999999
+            for i in range(len(visited)):
+                if visited[i]:
+                    distance = abs(y-coor_2[i][0]) + abs(x-coor_2[i][1])
+                    min_distance = min(distance, min_distance)
+            ans += min_distance
+        result = min(result, ans)
+        return
+        
+    for i in range(idx, len(coor_2)):
+        if not visited[i]:
+            visited[i] = True
+            dfs(i+1, cnt+1)
+            visited[i] = False
+    
+dfs(0, 0)
+print(result)
