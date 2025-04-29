e48b70723bc032f4efbbe784315f6eff26325007
KIMMINUK <kmwpcpk@gmail.com>
2025-04-30 03:21:42 +0900

    0430

--- Pass/7568.py
+++ Pass/7568.py
@@ -4,10 +4,17 @@
 
 n = int(input())
 dungchi = []
+rank = [1] * n
 
 for _ in range(n):
    dungchi.append(list(map(int, input().split()))) 
 
-dungchi.sort(reverse=True)
+for i in range(n):
+    for j in range(n):
+        if i == j:
+            continue
+        if dungchi[i][0] < dungchi[j][0] and dungchi[i][1] < dungchi[j][1]:
+            rank[i] += 1
 
-print(dungchi)
+for i in rank:
+    print(i, end=" ")
