0a4b8f33412f47aa12f20dc3fa30126b1c4248dc
KIMMINUK <kmwpcpk@gmail.com>
2025-05-02 21:23:30 +0900

    0502

--- Pass/9012.py
+++ Pass/9012.py
@@ -3,5 +3,27 @@ vps = []
 for _ in range(n):
     vps.append(list(input().strip()))
 
-
-            
+for i in range(n):
+    idx = 0
+    q = []
+    is_vps = True
+    while idx < n:
+        if vps[i][idx] == '(':
+            q.append(vps[i][idx])
+            idx += 1
+        else:
+            if not q:
+                print('hi')
+                is_vps = False
+                break
+            while q:
+                if vps[i][idx] != ')':
+                    idx = n
+                    is_vps = False
+                    break
+                q.pop()
+                idx += 1
+    if is_vps:
+        print('YES')
+    else:
+        print('NO')
