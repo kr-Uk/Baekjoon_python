e97983a3cb6a175cf2fa1728654615789df73af9
KIMMINUK <kmwpcpk@gmail.com>
2025-05-02 21:28:38 +0900

    0502

--- Pass/9012.py
+++ Pass/9012.py
@@ -4,25 +4,19 @@ for _ in range(n):
     vps.append(list(input().strip()))
 
 for i in range(n):
-    idx = 0
     q = []
     is_vps = True
-    while idx < n:
-        if vps[i][idx] == '(':
-            q.append(vps[i][idx])
-            idx += 1
+    for j in vps[i]:
+        if j == '(':
+            q.append('(')
         else:
             if not q:
-                print('hi')
                 is_vps = False
                 break
-            while q:
-                if vps[i][idx] != ')':
-                    idx = n
-                    is_vps = False
-                    break
+            else:
                 q.pop()
-                idx += 1
+    if q:
+        is_vps = False
     if is_vps:
         print('YES')
     else:
