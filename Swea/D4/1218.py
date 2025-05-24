b5677b0e817192d5b203c20637699e5877d25342
KIMMINUK <kmwpcpk@gmail.com>
2025-05-24 19:32:57 +0900

    0524

--- Swea/D4/1218.py
+++ Swea/D4/1218.py
@@ -7,27 +7,26 @@ for t in range(1, 11):
     gwalho = list(input())
     result = True
     for i in range(n):
-        for j in range(4):
-            if gwalho[i] == '(':
-               _type['('] += 1
-            elif gwalho[i] == '[':
-               _type['['] += 1
-            elif gwalho[i] == '{':
-               _type['{'] += 1
-            elif gwalho[i] == '<':
-               _type['<'] += 1
-            elif gwalho[i] == ')':
-               _type[')'] += 1
-            elif gwalho[i] == ']':
-               _type[']'] += 1
-            elif gwalho[i] == '}':
-               _type['}'] += 1
-            elif gwalho[i] == '>':
-               _type['>'] += 1
-        for i in range(4):
-            if _type[_type2[i]] != _type[_type2[i+4]]:
-                result = False
-        if result:
-            print(MSG_FORMAT.format(t, 1))
-        else:
-            print(MSG_FORMAT.format(t, 0))
+        if gwalho[i] == '(':
+            _type['('] += 1
+        elif gwalho[i] == '[':
+            _type['['] += 1
+        elif gwalho[i] == '{':
+            _type['{'] += 1
+        elif gwalho[i] == '<':
+            _type['<'] += 1
+        elif gwalho[i] == ')':
+            _type[')'] += 1
+        elif gwalho[i] == ']':
+            _type[']'] += 1
+        elif gwalho[i] == '}':
+            _type['}'] += 1
+        elif gwalho[i] == '>':
+            _type['>'] += 1
+    for i in range(4):
+        if _type[_type2[i]] != _type[_type2[i+4]]:
+            result = False
+    if result:
+        print(MSG_FORMAT.format(t, 1))
+    else:
+        print(MSG_FORMAT.format(t, 0))
