aaa0af42efe35efea3903a9c29f6018029c6d352
KIMMINUK <kmwpcpk@gmail.com>
2025-05-28 20:43:21 +0900

    0528

--- Part1/3460.py
+++ Part1/3460.py
@@ -2,3 +2,13 @@ t = int(input())
 
 for _ in range(t):
     n = int(input())
+    ilst = []
+    while n != 1:
+        ilst.append(n%2)
+        n = n//2
+    ilst.append(1)
+    
+    for i in range(len(ilst)):
+        if ilst[i] == 1:
+            print(i, end = " ")
+    print()
