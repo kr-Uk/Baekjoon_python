f2a1a43c347b542e3a7e73b5fc0ec505bba546c1
KIMMINUK <kmwpcpk@gmail.com>
2025-06-13 00:39:12 +0900

    0613

--- Part2/retry/16916.py
+++ Part2/retry/16916.py
@@ -1,10 +1,33 @@
 import sys
 input = sys.stdin.readline
 
-s = input().strip()
-p = input().strip()
+s = input().rstrip()
+p = input().rstrip()
 pi = [0] * len(p)
 pi[0] = 0
 
 for i in range(1, len(p)):
-    pass
+    tmp = p[:i+1]
+    for j in range((i+1)//2):
+        if tmp[:j+1] == tmp[-(j+1):]:
+            pi[i] = j+1
+
+idx = 0
+
+while idx <= len(s) - len(p):
+    cnt = -1
+    for i in range(len(p)):
+        if s[idx+i] == p[i]:
+            cnt += 1
+        else:
+            break
+        
+    if cnt == len(p)-1:
+        print(1)
+        exit(0)
+    if cnt > 0 and pi[cnt] > 0:
+        idx += len(p) - pi[cnt]
+    else:
+        idx += 1
+        
+print(0)
