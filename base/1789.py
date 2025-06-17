107ba65e037f95b8ca513446871e5c940800ca53
KIMMINUK <kmwpcpk@gmail.com>
2025-06-17 14:15:22 +0900

    0617

--- base/1789.py
+++ base/1789.py
@@ -2,7 +2,10 @@ n = int(input())
 
 t = 1
 
-while not (t*(t-1)/2  < n < t*(t+1)/2):
-    t += 1
-    
+while not (t*(t-1)  < 2 * n < t*(t+1)):
+    if 2 * n < t*(t-1):
+        t = t//2
+    else:
+        t += 1
+        
 print(t-1)
