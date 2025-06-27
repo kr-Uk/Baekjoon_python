878968e959cc84ef0510b7549b18354908140bb5
KIMMINUK <kmwpcpk@gmail.com>
2025-06-27 23:48:46 +0900

    0627

--- base/retry/17070.py
+++ base/retry/17070.py
@@ -1,3 +1,4 @@
+"""
 import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(10**9)
@@ -11,7 +12,7 @@ cnt = 0
 dx = [1, 0, 1]
 dy = [0, 1, 1]
 
-def dfs(pipe, x, y):
+def dfs(isWidth, isHeight, x, y):
     global cnt
     
     if x == n-1 and y == n-1:
@@ -22,13 +23,23 @@ def dfs(pipe, x, y):
         nx = x + dx[i]
         ny = y + dy[i]
         if 0 <= nx < n and 0 <= ny < n and graph[ny][nx] == 0:
-            if i == 0 and pipe in ['width', 'diag']:
-                dfs('width', nx, ny)
-            if i == 1 and pipe in ['height', 'diag']:
-                dfs('height', nx, ny)
+            if i == 0 and isWidth:
+                dfs(True, False, nx, ny)
+            if i == 1 and isHeight:
+                dfs(False, True, nx, ny)
             if i == 2 and graph[ny-1][nx] == 0 and graph[ny][nx-1] == 0:
-                dfs('diag', nx, ny)
+                dfs(True, True, nx, ny)
         
     
-dfs('width', 1, 0)
+dfs(True, False, 1, 0)
 print(cnt)
+"""
+
+import sys
+input = sys.stdin.readline
+
+graph = []
+n = int(input())
+for _ in range(n):
+    graph.append(list(map(int, input().split())))
+    
