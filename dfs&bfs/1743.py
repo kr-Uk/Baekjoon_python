7aaa2828bd34bb267c48b8839bebcc72e35ca7a9
KIMMINUK <kmwpcpk@gmail.com>
2025-07-10 20:41:37 +0900

    0710

--- dfs&bfs/1743.py
+++ dfs&bfs/1743.py
@@ -1,37 +1,35 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
+from collections import deque
 
-n, m, k = map(int, input().split())
-graph = [[0] * m for _ in range(n)]
-result = 0
-
-for _ in range(k):
-    x, y = map(int, input().split())
-    graph[y-1][x-1] = 1
-    
 def bfs(x, y):
     cnt = 1
-    q = deque([(x, y)])
-    graph[y][x] = 0
     dx = [1, -1, 0, 0]
     dy = [0, 0, 1, -1]
-    
+    q = deque([(x, y)])
     while q:
-        _x, _y = q.popleft()
+        x, y = q.popleft()
         for i in range(4):
-            nx = _x + dx[i]
-            ny = _y + dy[i]
-            if 0 <= nx < m and 0 <= ny < n and graph[ny][nx] == 1:
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 < nx < m+1 and 0 < ny < n+1 and graph[ny][nx] == 1:
                 graph[ny][nx] = 0
                 cnt += 1
                 q.append((nx, ny))
-    
     return cnt
 
-for y in range(n):
-    for x in range(m):
+n, m, k = map(int, input().split())
+graph = [[0] * (m+1) for _ in range(n+1)]
+result = 0
+
+for _ in range(k):
+    col, row = map(int, input().split())
+    graph[col][row] = 1
+
+for x in range(1, m+1):
+    for y in range(1, n+1):
         if graph[y][x] == 1:
+            graph[y][x] = 0
             result = max(result, bfs(x, y))
 
 print(result)
